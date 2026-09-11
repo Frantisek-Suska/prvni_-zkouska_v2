3c31fea3782dde3291c57154d1539cad1343182e
Frantisek-Suska <frantisek.suska.025@pslib.cz>
2026-09-11 13:14:28 +0200

    Add files via upload

---
+++ čísla.cs
@@ -0,0 +1,24 @@
+﻿int step;
+int count;
+
+Console.WriteLine("enter start value:");
+string? start = Console.ReadLine();
+
+Console.WriteLine("enter step value:");
+string? step = Console.ReadLine();
+
+Console.WriteLine("enter count value:");
+string? count = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
